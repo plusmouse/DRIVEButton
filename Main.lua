67a2a4d39f97c3e83ea1ad8f981f3939d3c7771d
plusmouse <plusmouse@protonmail.com>
2025-03-03 12:58:30 +0000

    Add slash command /drive and prevent button showing on characters without DRIVE

--- Main.lua
+++ Main.lua
@@ -2,42 +2,48 @@
 -- Copyright (c) plusmouse 2025
 
 local DRIVE_TREE = 1056
+local function OpenDRIVE()
+  if GenericTraitFrame and GenericTraitFrame:IsShown() then
+    GenericTraitFrame:Hide()
+    return
+  end
+  C_AddOns.LoadAddOn("Blizzard_GenericTraitUI");
+  if tIndexOf(UISpecialFrames, "GenericTraitFrame") == nil then
+    table.insert(UISpecialFrames, "GenericTraitFrame")
+  end
+  GenericTraitFrame:SetTreeID(DRIVE_TREE);
+  GenericTraitFrame:Show();
+  GenericTraitFrame:SetPoint("CENTER");
+end
+
 EventUtil.ContinueOnAddOnLoaded("DRIVEButton", function()
+  DRIVE_BUTTON_MINIMAP = DRIVE_BUTTON_MINIMAP or {}
+
   local buttonLDB = LibStub("LibDataBroker-1.1"):NewDataObject("DRIVEButton", {
     type = "launcher",
     text = "DRIVE",
     icon = "interface/icons/inv_misc_punchcards_prismatic",
-    OnClick = function()
-      if GenericTraitFrame and GenericTraitFrame:IsShown() then
-        GenericTraitFrame:Hide()
-        return
-      end
-      C_AddOns.LoadAddOn("Blizzard_GenericTraitUI");
-      if tIndexOf(UISpecialFrames, "GenericTraitFrame") == nil then
-        table.insert(UISpecialFrames, "GenericTraitFrame")
-      end
-      GenericTraitFrame:SetTreeID(DRIVE_TREE);
-      GenericTraitFrame:Show();
-      GenericTraitFrame:SetParent(UIParent);
-      GenericTraitFrame:SetPoint("CENTER");
-    end,
+    OnClick = OpenDRIVE,
     OnTooltipShow = function(tooltip)
       tooltip:AddLine("DRIVE")
     end,
   })
 
-  local icon = LibStub("LibDBIcon-1.0")
-  icon:Register("DRIVEButton", buttonLDB, {hide = true})
-
+  local registered = false
   -- Only show DRIVE button when character has it enabled
   local watcher = CreateFrame("Frame")
   watcher:RegisterEvent("TRAIT_CONFIG_LIST_UPDATED")
   watcher:RegisterEvent("PLAYER_LOGIN")
   watcher:SetScript("OnEvent", function()
+    if registered then
+      return
+    end
     if C_Traits.GetConfigIDByTreeID(DRIVE_TREE) then
-      icon:Show("DRIVEButton")
-    else
-      icon:Hide("DRIVEButton")
+      local icon = LibStub("LibDBIcon-1.0")
+      icon:Register("DRIVEButton", buttonLDB, DRIVE_BUTTON_MINIMAP)
+      SlashCmdList["DRIVEButton"] = OpenDRIVE
+      SLASH_DRIVEButton1 = "/drive"
+      registered = true
     end
   end)
 
